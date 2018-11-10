2696a6cec6a25b595425aa1a914cb3add685873c
unknown <lokesh.panala@gmail.com>
2018-11-10 20:28:59 +0530

    updated test file

--- src/test/java/Helloworld/AppTest.java
+++ src/test/java/Helloworld/AppTest.java
@@ -1,22 +1,32 @@
+import org.junit.Rule;
+import org.junit.rules.ExpectedException;
 import org.junit.Test;
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 public class AppTest
 {
-	//String msg="hello world";
-	//HelloWorld h=new HelloWorld();
+	
+	String s="hey";
+	HelloWorld h=new HelloWorld();
+	@Test
 	public void test1()
 	{
-		HelloWorld.sayHello(null);
 		
+		String s1=h.sayHello(s);
+		assertEquals(s,s1);
 	}
+	
+	@Test
 	public void test2()
 	{
-		HelloWorld.sayHello("");
+		
+		String s2=h.sayHello(null);
+		assertEquals(s2,"sayHello cant take an empty or null string");
 	}
+	@Test
 	public void test3()
 	{
-		HelloWorld.sayHello("hello world");
-		
+		String s3=h.sayHello("");
+		assertEquals(s3,"sayHello cant take an empty or null string");
 	}
-}
 	
+}
