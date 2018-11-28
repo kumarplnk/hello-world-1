6478a93bb8eeac39b2155628a713590c0dc067a5
unknown <lokesh.panala@gmail.com>
2018-11-28 17:46:45 +0530

    updated src file dated 28/11

--- src/main/java/Helloworld/HelloWorld.java
+++ src/main/java/Helloworld/HelloWorld.java
@@ -1,14 +1,14 @@
 class HelloWorld
 {
-	static String VAL="hello world ";
+	static String val="hello world ";
 	public String sayHello(String msg) throws IllegalArgumentException
 	{
 		if(msg==null || msg.isEmpty()||msg.trim().isEmpty())
 		{
 			throw new IllegalArgumentException("Argument cant be null or empty");
 		}
-			VAL+=msg;
-			System.out.println(VAL);
-			return VAL;
+			val+=msg;
+			System.out.println(val);
+			return val;
 	}
 }
